b69194665edb58acb4b8475ab637661f39331639
Leonardo Jose da Silva Ahmed <leonardo.ahmed@unigranrio.br>
2020-06-01 13:31:46 -0300

    Enviando commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
   }
 ];
 
